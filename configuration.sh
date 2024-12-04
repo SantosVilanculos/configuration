8d4ef5d9c1e532784545d0457cd1da53f600945e
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-04 06:08:41 +0200

    feat: microsoft windows support

--- configuration.sh
+++ configuration.sh
@@ -2,25 +2,32 @@
 
 WORKDIR=$(dirname $(realpath "$0"))
 
+ln -fns "$WORKDIR/.profile" "$HOME/.profile"
 ln -fns "$WORKDIR/.bashrc" "$HOME/.bashrc"
-
-ln -fns "$WORKDIR/.zshenv" "$HOME/.zshenv"
-ln -fns "$WORKDIR/.zshrc" "$HOME/.zshrc"
-
-ln -fns "$WORKDIR/.gitconfig" "$HOME/.gitconfig"
-
-mkdir -p "$HOME/.config/gh"
-ln -fns "$WORKDIR/gh/config.yml" "$HOME/.config/gh/config.yml"
-
-ln -fns "$WORKDIR/.tmux.conf" "$HOME/.tmux.conf"
-
-ln -fns "$WORKDIR/alacritty" "$HOME/.config/alacritty"
-
-ln -fns "$WORKDIR/i3" "$HOME/.config/i3"
-
-mkdir -p "$HOME/.config/Code/User"
-ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
 
 mkdir -p "$HOME/.local/bin"
 chmod +x "$WORKDIR/.local/bin/update"
 ln -fns "$WORKDIR/.local/bin/update" "$HOME/.local/bin/update"
+
+ln -fns "$WORKDIR/.gitconfig" "$HOME/.gitconfig"
+
+if [[ "$OSTYPE" == linux-gnu* ]]; then
+    ln -fns "$WORKDIR/.zshenv" "$HOME/.zshenv"
+    ln -fns "$WORKDIR/.zshrc" "$HOME/.zshrc"
+
+    mkdir -p "$HOME/.config/gh"
+    ln -fns "$WORKDIR/gh/config.yml" "$HOME/.config/gh/config.yml"
+
+    ln -fns "$WORKDIR/.tmux.conf" "$HOME/.tmux.conf"
+
+    ln -fns "$WORKDIR/alacritty" "$HOME/.config/alacritty"
+
+    ln -fns "$WORKDIR/i3" "$HOME/.config/i3"
+
+    mkdir -p "$HOME/.config/Code/User"
+    ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
+
+elif [[ "$OSTYPE" == cygwin* || "$OSTYPE" == msys* ]]; then
+    mkdir -p "$HOME/AppData/Roaming/Code/User"
+    ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/AppData/Roaming/Code/User/settings.json"
+fi
