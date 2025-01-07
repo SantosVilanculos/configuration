b4feb48ab47ea5cc5cc590f9ae2240f117b2da41
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-07 12:14:24 +0200

    style: use 2 spaces

--- configuration.sh
+++ configuration.sh
@@ -16,33 +16,33 @@ ln -fns "$WORKDIR/bin/favicon" "$HOME/.local/bin/favicon"
 ln -fns "$WORKDIR/.gitconfig" "$HOME/.gitconfig"
 
 if [[ "$OSTYPE" == linux-gnu* ]]; then
-    ln -fns "$WORKDIR/.zshenv" "$HOME/.zshenv"
-    ln -fns "$WORKDIR/.zshrc" "$HOME/.zshrc"
+  ln -fns "$WORKDIR/.zshenv" "$HOME/.zshenv"
+  ln -fns "$WORKDIR/.zshrc" "$HOME/.zshrc"
 
-    ln -fns "$WORKDIR/rofi" "$HOME/.config/rofi"
+  ln -fns "$WORKDIR/rofi" "$HOME/.config/rofi"
 
-    mkdir -p "$HOME/.config/gh"
-    ln -fns "$WORKDIR/gh/config.yml" "$HOME/.config/gh/config.yml"
+  mkdir -p "$HOME/.config/gh"
+  ln -fns "$WORKDIR/gh/config.yml" "$HOME/.config/gh/config.yml"
 
-    ln -fns "$WORKDIR/.tmux.conf" "$HOME/.tmux.conf"
+  ln -fns "$WORKDIR/.tmux.conf" "$HOME/.tmux.conf"
 
-    ln -fns "$WORKDIR/alacritty" "$HOME/.config/alacritty"
-    ln -fns "$WORKDIR/ghostty" "$HOME/.config/ghostty"
+  ln -fns "$WORKDIR/alacritty" "$HOME/.config/alacritty"
+  ln -fns "$WORKDIR/ghostty" "$HOME/.config/ghostty"
 
-    ln -fns "$WORKDIR/i3" "$HOME/.config/i3"
+  ln -fns "$WORKDIR/i3" "$HOME/.config/i3"
 
-    ln -fns "$WORKDIR/nvim" "$HOME/.config/nvim"
+  ln -fns "$WORKDIR/nvim" "$HOME/.config/nvim"
 
-    mkdir -p "$HOME/.config/Code/User"
-    ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
+  mkdir -p "$HOME/.config/Code/User"
+  ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
 
-    if [ -f /usr/local/bin/mailpit ]; then
-        sudo cp "$WORKDIR/mailpit.service" /etc/systemd/system/mailpit.service
-        sudo systemctl enable mailpit.service
-        sudo systemctl start mailpit.service
-    fi
+  if [ -f /usr/local/bin/mailpit ]; then
+    sudo cp "$WORKDIR/mailpit.service" /etc/systemd/system/mailpit.service
+    sudo systemctl enable mailpit.service
+    sudo systemctl start mailpit.service
+  fi
 
 elif [[ "$OSTYPE" == cygwin* || "$OSTYPE" == msys* ]]; then
-    mkdir -p "$HOME/AppData/Roaming/Code/User"
-    ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/AppData/Roaming/Code/User/settings.json"
+  mkdir -p "$HOME/AppData/Roaming/Code/User"
+  ln -fns "$WORKDIR/Code/User/settings.json" "$HOME/AppData/Roaming/Code/User/settings.json"
 fi
