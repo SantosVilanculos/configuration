31833d374eafdb394aa2eaea8c45fba5e796c0f5
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-10 12:44:14 +0200

    style: format code

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,8 @@
-return
-{
-    {
-        "nvim-lualine/lualine.nvim",
-        opts = function(_, opts)
-            opts.sections.lualine_z = {}
-        end,
-    }
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {}
+    end,
+  },
 }
