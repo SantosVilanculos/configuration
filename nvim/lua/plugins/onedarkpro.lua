8c4ce9c2a43dee35d98263925135abbfbfd903a6
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-14 18:30:01 +0200

    chore: add onedarkpro.nvim

---
+++ nvim/lua/plugins/onedarkpro.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- onedark
+      -- onelight
+      -- onedark_vivid
+      -- onedark_dark
+      colorscheme = "onedark",
+    },
+  },
+}
