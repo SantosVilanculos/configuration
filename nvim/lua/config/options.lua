08c9f63ddc19bef6201a4b403d178cf47f5849ed
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-14 19:36:15 +0200

    chore: configure default php and python lsp

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_php_lsp = "intelephense"
+vim.g.lazyvim_python_lsp = "pyright"
+
 -- vim.o.winbar = "%=%m %f"
 -- vim.o.winbar = "%=%m %t"
 vim.o.relativenumber = false
