3275ee2de524e0c6cd1aa1c58c025f92c0826b27
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-14 18:28:29 +0200

    chore: add winbar configuration

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.relativenumber = false
-vim.opt.wrap = true
+-- vim.o.winbar = "%=%m %f"
+-- vim.o.winbar = "%=%m %t"
+vim.o.relativenumber = false
+vim.o.wrap = true
