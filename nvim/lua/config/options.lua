53b579cb1022ee0bc633f48147430821a0f21671
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 03:41:45 +0200

    chore(nvim): update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,10 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_cmp = "blink.cmp"
+vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_python_lsp = "pyright"
 
--- vim.o.winbar = "%=%m %f"
--- vim.o.winbar = "%=%m %t"
-vim.o.relativenumber = false
-vim.o.wrap = true
+vim.opt.relativenumber = false
+-- vim.opt.winbar = "%=%m %f"
+-- vim.opt.winbar = "%=%m %t"
+-- vim.opt.wrap = true
