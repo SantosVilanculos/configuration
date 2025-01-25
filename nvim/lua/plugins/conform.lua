d39f65880d03de1ae11e4acc414eceb8b66c509a
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 03:37:15 +0200

    chore(nvim): update conform.lua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,13 +3,30 @@ return {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
-      formaters_by_ft = {
-        sh = { "shfmt" },
-        lua = { "stylua" },
+      formatters_by_ft = {
+        bash = { "shfmt" },
+        css = { "prettier" },
+        html = { "prettier" },
+        javascript = { "prettier" },
         json = { "jq" },
+        lua = { "stylua" },
         php = { "pint" },
         python = { "isort", "black" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
       },
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, null_ls.builtins.formatting.pint)
+      table.insert(opts.sources, null_ls.builtins.formatting.isort)
+      table.insert(opts.sources, null_ls.builtins.formatting.black)
+    end,
   },
 }
