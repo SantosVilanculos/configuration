edfcb086ce3b26d1c9cafaabb3f4dc187ed1d54e
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 03:40:50 +0200

    chore(nvim): update bufferline.lua

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,10 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
     opts = {
       options = {
         always_show_bufferline = true,
