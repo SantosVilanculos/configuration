fef4cdf27bc92438ce349c2d6900ea948b289a0a
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 03:35:56 +0200

    chore(nvim): update nvim-lint.lua

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,11 +1,18 @@
 return {
   {
     "mfussenegger/nvim-lint",
-    optional = true,
     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
     opts = {
       linters_by_ft = {
+        bash = { "shellcheck" },
+        css = { "eslint_d" },
+        html = { "htmlhint" },
+        javascript = { "eslint_d" },
         php = { "phpstan" },
+        python = { "ruff" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        vue = { "eslint_d" },
         ["*"] = { "cspell" },
       },
     },
