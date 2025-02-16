07fcd5c567630c336978474215f84dcd39ec473f
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-02-17 00:24:44 +0200

    chore(nvim): disable backup and swapfile in favor of undofile

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,7 +7,15 @@ vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_python_lsp = "pyright"
 
-vim.opt.relativenumber = false
--- vim.opt.winbar = "%=%m %f"
--- vim.opt.winbar = "%=%m %t"
--- vim.opt.wrap = true
+-- vim.o.winbar = "%=%m %f"
+-- vim.o.winbar = "%=%m %t"
+vim.o.relativenumber = false
+-- vim.o.wrap = true
+
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.swapfile = false
+-- vim.o.backupdir = '/tmp/'
+vim.o.undodir = "/tmp/"
+-- vim.o.directory = "/tmp/"
